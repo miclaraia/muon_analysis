4e663c5d59c2653f4c506280b4c2d6d89de2b5f3
Michael Laraia <micheal.laraia@gmail.com>
2017-10-25 15:02:54 -0500

    Added script to upload subject metadata

---
+++ muon/swap/muon_metadata.py
@@ -0,0 +1,74 @@
+
+from swap.db import DB
+
+import argparse
+import csv
+import json
+import re
+
+import logging
+logger = logging.getLogger(__name__)
+
+parse = re.compile('run([0-9]+)_evt([0-9]+).jpeg')
+
+
+def options(parser):
+    parser.add_argument('file', help='subject csv dump')
+
+
+def run(args):
+    fname = args.file[0]
+    data = get_metadata(fname)
+    upload_data(data)
+
+
+def upload_data(data):
+    db = DB()
+    requests = []
+
+    def write():
+        nonlocal requests
+        if len(requests) > 0:
+            db.subjects.bulk_write(requests)
+            requests = []
+
+    for subject, metadata in data.items():
+        r = db.subjects.update_metadata(subject, metadata, False)
+        requests.append(r)
+
+        if len(requests) > 1e4:
+            write()
+    write()
+
+
+def get_metadata(fname):
+    data = {}
+
+    with open(fname, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            subject, metadata = parse_row(row)
+            data[subject] = metadata
+
+    return data
+
+
+def parse_row(row):
+    meta = json.loads(row['metadata'])
+
+    subject = row['subject_id']
+    fname = meta['Filename']
+
+    run_, evt = parse_fname(fname)
+
+    metadata = {
+        'run': run_,
+        'event': evt,
+    }
+
+    return subject, metadata
+
+
+def parse_fname(fname):
+    run_, evt = parse.match(fname).groups()
+    return run_, evt
