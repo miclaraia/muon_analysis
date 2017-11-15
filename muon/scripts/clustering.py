fc5754adf48c935b94b4b0c697a8324362287549
Michael Laraia <micheal.laraia@gmail.com>
2017-11-15 10:20:46 -0600

    Updated script for region subject images

--- muon/scripts/clustering.py
+++ muon/scripts/clustering.py
@@ -11,11 +11,14 @@ def regions(path):
     subjects = clustering.Subjects(path)
     cluster = clustering.Cluster.create(subjects)
 
-    download(cluster, (-1.21084, 2.60831), (-1.87769, 1.00803), None,
+    print('Middle region')
+    download(cluster, (-1.21084, 3.0), (-1.87769, 1.00803), None,
              100, 'red_region', './middle')
+    print('Left region')
     download(cluster, (-20, -1.3), None, None,
              100, 'blue_region', './left')
-    download(cluster, (-20, -1.3), None, None,
+    print('Right region')
+    download(cluster, (5, 100), None, None,
              100, 'outer_region', './right')
 
 
