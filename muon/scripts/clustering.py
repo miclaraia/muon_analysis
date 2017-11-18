a41b6a2fe73e95a3ef89ddfc1a27f89501ad6537
Michael Laraia <micheal.laraia@gmail.com>
2017-11-17 19:37:42 -0600

    Updated region plotting script
    Download and plot images of unlabeled images only

--- muon/scripts/clustering.py
+++ muon/scripts/clustering.py
@@ -2,24 +2,23 @@
 import muon.utils.clustering as clustering
 import swap.config
 
+import matplotlib.pyplot as plt
+
 def download(cluster, x, y, c, size, prefix, dir_):
     subjects = cluster.subjects_in_range(x, y, c)
     cluster.download_subjects(subjects, size, prefix, dir_)
 
-def regions(path):
-    swap.config.logger.init()
-    subjects = clustering.Subjects(path)
-    cluster = clustering.Cluster.create(subjects)
-
+def regions(cluster, path):
     print('Middle region')
-    download(cluster, (-1.21084, 3.0), (-1.87769, 1.00803), None,
-             100, 'red_region', './middle')
+    download(cluster, (-1.21084, 3.0), (-1.87769, 1.00803), -1,
+             200, 'red_region', './%s/middle' % path)
     print('Left region')
-    download(cluster, (-20, -1.3), None, None,
-             100, 'blue_region', './left')
+    download(cluster, (-20, -1.3), None, -1,
+             200, 'blue_region', './%s/left' % path)
     print('Right region')
-    download(cluster, (5, 100), None, None,
-             100, 'outer_region', './right')
+    download(cluster, (5, 100), None, -1,
+             200, 'outer_region', './%s/right' % path)
+
 def plot_classes(cluster, fname=None):
     fig = plt.figure()
     for i, s in enumerate((1, 0, -1)):
