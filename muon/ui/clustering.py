2662676438f252b33fd646129a648925dc279ecc
Michael Laraia <micheal.laraia@gmail.com>
2017-11-17 19:38:29 -0600

    Updated ui

--- muon/ui/clustering.py
+++ muon/ui/clustering.py
@@ -7,6 +7,10 @@ import swap.config
 import click
 import code
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+logger = logging.getLogger(__name__)
 
 
 @ui.cli.group()
@@ -15,22 +19,44 @@ def pca():
 
 
 def interact(local):
-    import numpy as np
-    import pickle
     def save(path):
         pickle.dump(local['cluster'], open(path, 'wb'))
 
-    code.interact(local=local)
+    code.interact(local={**globals(), **locals(), **local})
 
 
 @pca.command()
 @click.argument('path', nargs=-1)
-def run(path):
+@click.option('--components', default=8, type=int)
+@click.option('--plot/--noplot', default=True)
+@click.option('--mle', is_flag=True)
+@click.option('--all', is_flag=True)
+@click.option('--save', nargs=1, type=str)
+def run(path, components, mle, plot, save, all):
+    if len(path) == 0:
+        print('No data files provided')
+        return
+
     swap.config.logger.init()
     subjects = Subjects.from_data(path)
+    logger.info('Done loading subjects')
 
-    cluster = Cluster.create(subjects)
-    cluster.plot()
+    if mle:
+        cluster = Cluster.create(subjects, 'mle')
+    if all:
+        cluster = Cluster.create(subjects, None)
+    else:
+        cluster = Cluster.create(subjects, components)
+    logger.info('Done creating cluster')
+
+    if plot:
+        try:
+            cluster.plot()
+        except Exception:
+            pass
+
+    if save:
+        pickle.dump(cluster, open(save, 'wb'))
 
     interact(locals())
 
@@ -39,6 +65,7 @@ def run(path):
 def load(path):
     cluster = pickle.load(open(path, 'rb'))
 
+    plot = lambda: scripts.plot_classes(cluster)
     interact(locals())
 
 @pca.command()
@@ -51,10 +78,8 @@ def visualize(path):
 
 
 @pca.command()
-@click.argument('path', nargs=-1)
-@click.argument('script', nargs=1)
-def cluster(path, script):
-    if script == '1':
-        scripts.regions(path)
-    
-
+@click.argument('cluster', nargs=1)
+@click.argument('path', nargs=1)
+def region(cluster, path):
+    cluster = pickle.load(open(cluster, 'rb'))
+    scripts.regions(cluster, path)
