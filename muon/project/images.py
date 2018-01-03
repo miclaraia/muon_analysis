9e2c46e196a7328d3664a1f24c343d4c2c4f70ec
Michael Laraia <micheal.laraia@gmail.com>
2018-01-03 16:18:30 -0600

    Fleshed out Images class functionality

--- muon/project/images.py
+++ muon/project/images.py
@@ -1,5 +1,10 @@
 import numpy as np
 import math
+import os
+import json
+from shutil import copyfile
+
+import muon.data
 
 
 class Image:
@@ -12,55 +17,93 @@ class Image:
 
     def dump(self):
         return {
-                'id': self.id,
-                'subjects': self.subjects,
-                'metadata': self.metadata,
-                'zooniverse_id': self.zoo_id
+            'id': self.id,
+            'subjects': self.subjects,
+            'metadata': self.metadata,
+            'zooniverse_id': self.zoo_id
         }
 
     @classmethod
     def load(cls, dumped):
         kwargs = {
-                'id_': dumped['id'],
-                'subjects': dumped['subjects'],
-                'metadata': dumped['metadata'],
-                'zoo_id': dumped['zooniverse_id'],
+            'id_': dumped['id'],
+            'subjects': dumped['subjects'],
+            'metadata': dumped['metadata'],
+            'zoo_id': dumped['zooniverse_id'],
         }
 
-        return cls(kwargs)
+        return cls(**kwargs)
 
 
 class Images:
     _image = Image
 
-    def __init__(self, cluster, save_dir=None, **kwargs):
-        self.subjects = subjects
-        self.images = None
+    def __init__(self, group, images, next_id, **kwargs):
+        self.images = images
         # TODO load existing structure to not duplicate ids
-        self.next_id = 0
-
-        self.save_dir = save_dir
+        self.next_id = next_id
+        self.group = group
 
         self.size = kwargs.get('image_size', 40)
         self.image_dim = kwargs.get('image_dim', (50, 50))
+        self.description = kwargs.get('description', None)
 
-    @property
-    def structure(self):
-        return self.generate_structure()
+    @classmethod
+    def new(cls, cluster, **kwargs):
+        group, next_id = cls.load_metadata()
+        images = cls(group, None, next_id, **kwargs)
+        images.generate_structure(cluster)
 
-    def generate_structure(self):
+        return images
+
+    @classmethod
+    def load_group(cls, group):
+        fname = cls._fname()
+        if os.path.isfile(fname):
+            with open(fname, 'r') as file:
+                data = json.load(file)
+
+        next_id = data['next_id']
+        data = data['groups'][group]
+
+        images = [i.load() for i in data['images']]
+        images = cls(group, images, next_id)
+
+        images.metadata(data['metadata'])
+        return images
+
+    def metadata(self, new=None):
+        if new is None:
+            return {
+                'size': self.size,
+                'dim': self.image_dim,
+                'group': self.group,
+                'description': self.description,
+            }
+        else:
+            self.size = new['size']
+            self.image_dim = new['dim']
+            self.group = new['group']
+            self.description = new['description']
+
+    @staticmethod
+    def _fname():
+        return muon.data.path('structure.json')
+
+    def generate_structure(self, cluster):
         """
         Generate a file detailing which subjects belong in which image
         and their location in the image.
 
         """
+        subjects = cluster.subjects
         images = []
         i = self.next_id
 
-        subjects = self.subjects.list()
+        subjects = subjects.list()
         l = len(subjects)
         w = math.ceil(l/self.size)
-        
+
         for n in range(w):
             a = n * self.size
             b = min(l, a + self.size)
@@ -73,28 +116,47 @@ class Images:
 
         return images
 
+    def save_group(self, images):
+        fname = self._fname()
+        if os.path.isfile(fname):
+            with open(fname, 'r') as file:
+                data = json.load(file)
 
-    def save_structure(self, images):
-        data = {}
-        data['images'] = [i.dump() for i in images]
+            copyfile(fname, fname+'.bak')
+        else:
+            data = {'groups': {}}
+
+        if self.group in data['groups']:
+            print('file contents: ', data)
+            raise Exception('Refusing to overwrite group (%d) in structure '
+                            'file' % self.group)
+
+        data['groups'][self.group] = {
+            'metadata': self.metadata(),
+            'images': [i.dump() for i in images]
+        }
+        data['next_id'] = self.next_id
+        data['next_group'] = self.group + 1
 
         # TODO save to different file per upload...? Or have them all in the
         # same file. Probably want them all in the same file.
-        fname = os.path.join(self.save_dir, 'structure.json')
         with open(fname, 'w') as file:
             json.dump(data, file)
 
+    @classmethod
+    def load_metadata(cls):
+        fname = cls._fname()
+        if os.path.isfile(fname):
+            with open(fname, 'r') as file:
+                data = json.load(file)
 
-    def load_structure(self):
-        fname = os.path.join(self.save_dir, 'structure.json')
-        with open(fname, 'r') as file:
-            data = json.load(file)
+            next_id = data['next_id']
+            group = data['next_group']
+        else:
+            next_id = 0
+            group = 0
 
-        I = self._image
-        images = []
-
-        
-
+        return group, next_id
 
     def generate_manifest(self):
         """
@@ -115,4 +177,5 @@ class Random_Images(Images):
         super().__init__(subjects, **kwargs)
 
     def _structure(self):
+        pass
 
