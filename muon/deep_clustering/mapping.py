66d43c89b5033a0fa3ab75afcf50b933bcc3b9e3
Michael Laraia <micheal.laraia@gmail.com>
2018-04-25 17:14:24 -0500

    Updated mapping

--- muon/deep_clustering/mapping.py
+++ muon/deep_clustering/mapping.py
@@ -1,6 +1,9 @@
 import sys
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+import matplotlib.colors
+from matplotlib.colors import LinearSegmentedColormap
 import random
 
 from dec_keras import DEC, cluster_acc
@@ -90,13 +93,15 @@ class MappingSubset:
 class Mapping:
 
     def __init__(self, cluster, agg, train, validate,
-                 mode='subsample', size=1000):
+                 mode='subsample', size=1000, batched=False, shuffle=False,
+                 weights=(0, 1)):
         subjects = cluster.subjects
         # agg.apply_labels(subjects)
         self.agg = agg
         self.cluster = cluster
 
-        self.mode = (mode, size)
+        self.mode = {'mode': mode, 'size': size, 'batched': batched,
+                     'shuffle': shuffle, 'weights': weights}
 
         self.train = MappingSubset(train)
         self.validate = MappingSubset(validate)
@@ -118,46 +123,75 @@ class Mapping:
     def get_xy_volunteer(self, subset):
         x = []
         y = []
-        mode, size = self.mode
-        if mode == 'aggregate':
-            for s in subset:
-                subject = self.cluster.subjects[s]
-                reduce = self.agg.reduce()[1]
-                x.append(subject.charge)
-                y.append(int(reduce[s]>.5))
-        elif mode == 'weightedagg':
-            for s in subset:
-                subject = self.cluster.subjects[s]
-                reduce = self.agg.reduce()[1]
-                if reduce[s] < .3 or reduce[s] > .7:
-                    x.append(subject.charge)
-                    y.append(int(reduce[s]>.5))
-        elif mode == 'subsample':
+        mode = self.mode['mode']
+        size = self.mode['size']
+        def get_xy(subset):
+            x = []
+            y = []
             for s in subset:
                 subject = self.cluster.subjects[s]
                 for a in self.agg.data['subjects'][s]:
-                    x.append(subject.charge)
+                    x.append(s)
                     y.append(a)
+            print(len(x))
+            return np.array(list(zip(x, y)))
 
-            sample = random.sample(range(len(x)), size)
-            x = [x[s] for s in sample]
-            y = [y[s] for s in sample]
+        def out(data):
+            x, y = zip(*data)
+            x = [self.cluster.subjects[s].charge for s in x]
+            return np.array(x), np.array(y)
 
-        return np.array(x), np.array(y)
+        if mode == 'aggregate':
+            reduce = self.agg.reduce()[1]
+            data = np.zeros((len(subset), 2))
+            for i, s in enumerate(subset):
+                subject = self.cluster.subjects[s]
+                data[i] = (s, int(reduce[s]>.5))
+
+        elif mode == 'weightedagg':
+            reduce = self.agg.reduce()[1]
+            x = []
+            y = []
+            a, b = self.mode['weights']
+            for s in subset:
+                subject = self.cluster.subjects[s]
+                if reduce[s] == a or reduce[s] == b:
+                    x.append(s)
+                    y.append(int(reduce[s]>.5))
+            data = np.array(list(zip(x, y)))
+
+        elif mode == 'subsample':
+            data = get_xy(subset)
+            sample = random.sample(range(len(data)), size)
+            data = data[sample, :]
+
+        elif mode == 'all':
+            data = get_xy(subset)
+
+        if self.mode['shuffle']:
+            np.random.shuffle(data)
+
+        if self.mode['batched']:
+            print('batching')
+            while len(data) > 0:
+                b = min(len(data), size)
+                print(len(data), b)
+                yield out(data[:b,:])
+                data = data[b:,:]
+            return
+        else:
+            print('not batching')
+            yield out(data)
 
     def _cluster_mapping(self):
         pred = self.cluster.predictions
         return list(pred.cluster_mapping['majority_class'])
 
-    def apply_mapping(self):
+    def _create_mapping_model(self):
         n_classes = len(self.classes)
         cluster_mapping = self._cluster_mapping()
 
-        x_train, y_train = self.train.get_xy()
-        x_val, y_val = self.validate.get_xy()
-
         k_initializer = MapInitializer(cluster_mapping, n_classes)
-
         a = Input(shape=(self.width,)) # input layer
         q = self.cluster.dec.model(a)
 
@@ -168,12 +202,33 @@ class Mapping:
         model = Model(inputs=a, outputs=pred)
         model.compile(loss='categorical_crossentropy', optimizer='adam')
 
+        return model
+
+
+    def apply_mapping(self, epochs=10):
+        n_classes = len(self.classes)
+        x_train, y_train = self.train.get_xy()
+        x_val, y_val = self.validate.get_xy()
+
+        model = self._create_mapping_model()
+
         def make_data(x, y):
             return (x, np_utils.to_categorical(y, n_classes))
 
-        x_data = make_data(*self.get_xy_volunteer(self.train.subset))
         val_data = make_data(x_val, y_val)
-        model.fit(*x_data, validation_data=val_data, epochs=10, batch_size=256)
+
+        tracking = []
+        for batch in self.get_xy_volunteer(self.train.subset):
+            x_batch = make_data(*batch)
+            print('batch', len(x_batch))
+            model.fit(*x_batch, validation_data=val_data,
+                      epochs=epochs, batch_size=256)
+
+            self.validate.predict(self.cluster)
+            tracking.append(self.validate.accuracy())
+            print('current validation: ', tracking[-1])
+        self.tracking = tracking
+        print(tracking)
 
         # Generate FOM score
         self.validate.predict(self.cluster)
@@ -207,30 +262,37 @@ class Mapping:
         fig = plt.figure(figsize=(6,6))
         ax = fig.add_subplot(111)
 
-        # define the colormap
-        cmap = plt.cm.jet
-        # extract all colors from the .jet map
-        cmaplist = [cmap(i) for i in range(cmap.N)]
-        # force the first color entry to be grey
-        cmaplist[0] = (.5,.5,.5,1.0)
-        # create the new map
-        cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
-
         if np.any(y):
             unique_targets = list(np.unique(y))
+            cmap = discrete_cmap(len(unique_targets), 'jet')
+            norm = matplotlib.colors.BoundaryNorm(
+                np.arange(0, max(unique_targets),1), cmap.N)
+
             if -1 in unique_targets:
                 _x = x_pca[np.where(y == -1), 0]
                 _y = x_pca[np.where(y == -1), 1]
                 ax.scatter(_x, _y, marker='o', s=20, c=ulcolour, alpha=0.1)
                 unique_targets.remove(-1)
             for l in unique_targets:
-                _x = x_pca[np.where(y == 1), 0]
-                _y = x_pca[np.where(y == 1), 1]
+                _x = x_pca[np.where(y == l), 0]
+                _y = x_pca[np.where(y == l), 1]
                 _c = l * np.ones(_x.shape)
                 ax.scatter(_x, _y, marker='o', s=5, c=_c,
-                           cmap='jet', alpha=0.2, label=labels[l])
+                           cmap=cmap, norm=norm, alpha=0.2, label=labels[l])
 
         else:
             ax.scatter(x_pca[:,0], x_pca[:,1], marker='o', s=20, \
                 color=ulcolour, alpha=0.1)
         plt.axis('off')
+
+def discrete_cmap(N, base_cmap=None):
+    """Create an N-bin discrete colormap from the specified input map"""
+
+    # Note that if base_cmap is a string or None, you can simply do
+    #    return plt.cm.get_cmap(base_cmap, N)
+    # The following works for string, None, or a colormap instance:
+
+    base = plt.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    return LinearSegmentedColormap.from_list(cmap_name, color_list, N)
