Fixed rotation bug for generating split

muon/scripts/data_management/subjects/generate_data_split.py
@@ -3,45 +3,65 @@ import random
 import numpy as np
 import click
 import json
+import pickle
 
 from muon.subjects.storage import Storage
 
 
 @click.group(invoke_without_command=True)
-@click.argument('subject_data')
-@click.argument('label_name')
-@click.argument('output_file')
-def main(subject_data, label_name, output_file):
+@click.option('--subject_data', required=True)
+@click.option('--label_name', required=True)
+@click.option('--splits_out', required=True)
+@click.option('--xy_out', required=True)
+@click.option('--rotation', is_flag=True)
+def main(subject_data, label_name, splits_out, xy_out, rotation):
     storage = Storage(subject_data)
     subjects = storage.get_subjects(storage.labeled_subjects(label_name))
+    storage.close()
 
-    keys = ['{}_{}'.format(s, n) for s in subjects.keys() for n in range(6)]
+    keys = [s for s in subjects.keys()]
+    if rotation:
+        keys = ['{}_{}'.format(s, n) for s in keys for n in range(6)]
     keys = np.random.permutation(keys)
 
-    split = {
+    splits = {
         'test': .25,
         'train': .4,
         'train_dev': .175,
         'valid': .175
     }
-    print('Sum of percentages: ', sum(split.values()))
+    print('Sum of percentages: ', sum(splits.values()))
 
     l = len(keys)
-    S = np.array(list(split.keys()))
-    N = np.round(np.array([split[k] for k in S]) * l, 0).astype(np.int)
+    S = np.array(list(splits.keys()))
+    N = np.round(np.array([splits[k] for k in S]) * l, 0).astype(np.int)
     print(N, sum(N), l)
 
-    split = {}
+    splits = {}
     for i, s in enumerate(S):
-        split[s] = keys[:N[i]]
+        splits[s] = keys[:N[i]]
         keys = keys[N[i]:]
 
-    print(sum([len(split[s]) for s in split]))
-    split = {k: list(split[k]) for k in split}
+    print(sum([len(splits[s]) for s in splits]))
+    splits = {k: list(splits[k]) for k in splits}
         
-    with open(output_file, 'w') as file:
-        json.dump(split, file)
+    with open(splits_out, 'w') as file:
+        json.dump(splits, file)
 
+    # Generate xy data
+    l = sum([len(splits[k]) for k in splits])
+    print([len(splits[s]) for s in splits])
+    for k in splits:
+        print(k)
+        splits[k] = subjects.get_xy(splits[k], label_name)
+        print(splits[k][0].shape)
+    l2 = sum([splits[k][0].shape[0] for k in splits])
+    print(l, l2)
+    print([splits[k][0].shape[0] for k in splits])
+    assert l == l2
+
+    with open(xy_out, 'wb') as f:
+        pickle.dump(splits, f)
 
 
 main()

muon/subjects/subjects.py
@@ -147,21 +147,14 @@ class Subjects:
     ###   Subject Charge Data   ##############################################
     ##########################################################################
 
-    def get_xy(self, rotation, label):
-        x = []
-        y = []
-        cr = CameraRotate()
-        for s in self.iter():
-            if rotation:
-                for n in range(6):
-                    x.append(cr.rotate(s.x, n))
-                    y.append(s.y[label])
-            else:
-                x.append(s.x)
-                y.append(s.y[label])
+    def get_xy(self, subjects, label):
+        x = np.zeros((len(subjects), self.get_dimensions()[1]))
+        y = np.zeros((len(subjects,)))
+        for i, s in enumerate(subjects):
+            s = self.get_subject(s)
+            x[i,:] = s.x
+            y[i] = s.y[label]
 
-        x = np.array(x)
-        y = np.array(y)
         return x, y
 
     ##########################################################################