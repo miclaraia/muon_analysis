dfedc4d22734c4cca3d33e1c99783b72b5140449
Michael Laraia <micheal.laraia@gmail.com>
2019-02-05 15:08:35 -0600

    .

--- muon/subjects/database.py
+++ muon/subjects/database.py
@@ -6,6 +6,7 @@ import json
 from muon.subjects.subject import Subject
 
 
+
 class Database:
 
     def __init__(self, fname):
@@ -88,7 +89,7 @@ class Database:
             self.conn.close()
 
 
-class Subjects:
+class SubjectStorage:
 
     @classmethod
     def next_id(cls, conn):
@@ -117,10 +118,27 @@ class Subjects:
             yield cls.add_subject(conn, subject)
 
     @classmethod
+    def add_subject_label(cls, conn, subject_id, label_name, label):
+        data = {
+            'subject_id': subject_id,
+            'label_name': label_name,
+            'label': label
+        }
+        keys, values = zip(*data.items())
+        conn.execute('INSERT INTO subject_labels ? VALUES ?', (keys, values))
+
+    @classmethod
+    def get_subject_label(cls, conn, subject_id, label_name):
+        cursor = conn.execute("""
+            SELECT label FROM subject_labels WHERE
+            subject_id=? AND label_name=?""", (subject_id, label_name))
+        return cursor.fetchone()[0]
+
+    @classmethod
     def get_subject(cls, conn, subject_id):
-        cursor = conn.execute(
-            'SELECT subject_id, run, evt, tel, charge FROM subjects' \
-            'WHERE subject_id=?', subject_id)
+        cursor = conn.execute("""
+            SELECT subject_id, run, evt, tel, charge FROM subjects
+            WHERE subject_id=?""", (subject_id,))
 
         row = cursor.fetchone()
         return Subject(
@@ -134,7 +152,7 @@ class Subjects:
         )
 
 
-class Image:
+class ImageStorage:
 
     @classmethod
     def next_id(cls, conn):
@@ -153,4 +171,85 @@ class Image:
         }
         keys, values = zip(*data.items())
         conn.execute('INSERT INTO images ? VALUEs ?', (keys, values))
+
+        return image.image_id
+
+    @classmethod
+    def get_image(cls, conn, image_id):
+        cursor = conn.execute("""
+            SELECT image_id, group_id, cluster, metadata, zoo_id
+            FROM images WHERE image_id=?""", (image_id,))
+        image = cursor.fetchone()
+
+        cursor = conn.execute("""
+            SELECT subject_id FROM image_subjects 
+            WHERE image_id=?""", (image_id,))
+        subjects = [row[0] for row in cursor]
+
+        return Image(
+            image_id=image[0],
+            group_id=image[1],
+            cluster=image[2],
+            metadata=json.loads(image[3]),
+            zoo_id=image[4],
+            subjects=subjects)
+
+    @classmethod
+    def get_group_images(cls, conn, group_id):
+        cursor = conn.execute(
+            'SELECT image_id FROM images WHERE group_id=?', (group_id,))
+        for row in cursor:
+            yield cls.get_image(conn, row[0])
+
+
+class ImageGroupStorage:
+
+    @classmethod
+    def next_id(cls, conn):
+        cursor = conn.execute('SELECT MAX(*) FROM groups')
+        return cursor.fetchone()[0] + 1
+
+    @classmethod
+    def delete_group(cls, conn, group_id):
+        print('Deleting group:', group_id, type(group_id))
+        conn.execute('DELETE FROM groups WHERE group_id=?', (group_id,))
+        conn.execute('DELETE FROM images WHERE group_id=?', (group_id,))
+        conn.execute('DELETE FROM subjects WHERE group_id=?', (group_id,))
+
+    @classmethod
+    def add_group(cls, conn, group):
+
+        group.group_id = cls.next_id(conn)
+        data = {
+            'group_id': group.group_id,
+            'image_size': group.image_size,
+            'image_width': group.image_width,
+            'description': group.description,
+            'permutations': group.permutations
+        }
+        keys, values = zip(*data.items())
+        conn.execute('INSERT INTO images ? VALUEs ?', (keys, values))
+
+        return group.group_id
+
+    @classmethod
+    def get_group(cls, conn, group_id):
+        cursor = conn.execute("""
+            SELECT group_id, image_size, image_width, description, permutations
+            FROM groups WHERE group_id=?""", (group_id,))
+        group = cursor.fetchone()
+
+        images = {image.id: image \
+                  for image in ImageStorage.get_group_images(conn, group_id)}
+
+        return ImageGroup(
+            group_id=group[0],
+            image_size=group[1],
+            image_width=group[2],
+            description=group[3],
+            permutations=group[4],
+            images=images)
+
+
+
 
