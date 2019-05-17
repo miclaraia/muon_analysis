43609e357fb68b8a313778b4697733e3ea92f6ac
Michael Laraia <micheal.laraia@gmail.com>
2019-05-17 12:25:35 -0500

    Updated image plotting

--- muon/images/single_image.py
+++ muon/images/single_image.py
@@ -50,42 +50,17 @@ class SingleImage(ImageParent):
         """
         return 's_muon_group_%d_id_%d.jpg' % (group_id, image_id)
 
-    def plot(self, width, subject_storage, dpi=None, quality=None, path=None):
+    def plot(self, width, subject_storage):
         """
         Generate and save a plot of this image
         """
-        fname = self.fname()
-        # Skip if image already exists
-        if fname in os.listdir(path):
-            return False
-        if path:
-            fname = os.path.join(path, fname)
-        # Touch the file
-        # so other concurrent image generation processes
-        # skip this image
+        subject = subject_storage.get_subject(self.subject)
+        fig = plt.figure(figsize=(1, 1))
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0,
+                            wspace=0)
+        ax = fig.add_subplot(111)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        subject.plot(ax)
 
-        try:
-            open(fname, 'w').close()
-
-            if quality is None:
-                quality = 95
-
-            subject = subject_storage.get_subject(self.subject)
-            fig = plt.figure(figsize=(1, 1))
-            fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0,
-                                wspace=0)
-            ax = fig.add_subplot(111)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            subject.plot(ax)
-
-            dpi = dpi or 300
-            self.image_meta = ImageParent.ImageMeta(dpi=dpi)
-
-            fig.savefig(fname, dpi=dpi, quality=quality)
-            plt.close(fig)
-            return True
-
-        except (KeyboardInterrupt, Exception):
-            os.remove(fname)
-            raise
+        return fig, {}
